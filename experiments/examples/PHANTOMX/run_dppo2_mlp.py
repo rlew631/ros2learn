391535d90b0f63a056b1e92c455e0a8ad1701857
Kai Konen <kkonen@uni-bielefeld.de>
2019-12-17 15:13:52 +0100

    changes in run dppo and submodules

--- experiments/examples/PHANTOMX/run_dppo2_mlp.py
+++ experiments/examples/PHANTOMX/run_dppo2_mlp.py
@@ -63,101 +63,6 @@ def make_env():
 
     return env
 
-
-def leg_obs(main_obs, leg_name):
-    obs = 0
-    if leg_name is 'lf':
-        # obs = np.delete(main_obs, np.s_[108:162])   MAKE THIS SMARTER! NUM_OF_VARIABLES PER LEG * LIMBS PER LEG!
-        # positions
-        obs = np.delete(main_obs, np.s_[18:27])
-        obs = np.delete(obs, np.s_[27:45])
-        # ground contacts
-        obs = np.delete(obs, np.s_[29])
-        obs = np.delete(obs, np.s_[30:32])
-        # actions
-        obs = np.delete(obs, np.s_[42:45])
-        obs = np.delete(obs, np.s_[45:51])
-        obs = np.delete(obs, np.s_[36:39])
-        # linear velocities
-        #obs = np.delete(obs, np.s_[72:90])
-        #obs = np.delete(obs, np.s_[54:63])
-
-    if leg_name is 'lm':
-        # obs = np.delete(main_obs, np.s_[81:108])
-        # obs = np.delete(obs, np.s_[108:135])
-        obs = np.delete(main_obs, np.s_[27:54])
-
-        obs = np.delete(obs, np.s_[30:33])
-
-        obs = np.delete(obs, np.s_[45:54])
-        obs = np.delete(obs, np.s_[39:42])
-
-        #obs = np.delete(obs, np.s_[63:90])
-
-
-    if leg_name is 'lr':
-        # obs = np.delete(main_obs, np.s_[81:135])
-        obs = np.delete(main_obs, np.s_[0:9])
-        obs = np.delete(obs, np.s_[18:36])
-
-        obs = np.delete(obs, np.s_[27])
-        obs = np.delete(obs, np.s_[29:31])
-
-        obs = np.delete(obs, np.s_[36:39])
-        obs = np.delete(obs, np.s_[42:48])
-        obs = np.delete(obs, np.s_[39:42])
-
-        #obs = np.delete(obs, np.s_[63:81])
-        #obs = np.delete(obs, np.s_[36:45])
-
-
-
-    if leg_name is 'rf':
-        # obs = np.delete(main_obs, np.s_[27:81])
-        obs = np.delete(main_obs, np.s_[9:27])
-        obs = np.delete(obs, np.s_[27:36])
-
-        obs = np.delete(obs, np.s_[28:30])
-        obs = np.delete(obs, np.s_[30])
-
-        obs = np.delete(obs, np.s_[39:45])
-        obs = np.delete(obs, np.s_[45:48])
-        obs = np.delete(obs, np.s_[39:42])
-
-        # obs = np.delete(obs, np.s_[81:90])
-        # obs = np.delete(obs, np.s_[45:63])
-
-
-    if leg_name is 'rm':
-        # obs = np.delete(main_obs, np.s_[0:27])
-        # obs = np.delete(obs, np.s_[27:54])
-        obs = np.delete(main_obs, np.s_[0:27])
- 
-        obs = np.delete(obs, np.s_[27:30])
- 
-        obs = np.delete(obs, np.s_[36:45])
-        obs = np.delete(obs, np.s_[39:42])
-
-        #obs = np.delete(obs, np.s_[36:63])
-
-
-    if leg_name is 'rr':
-        # obs = np.delete(main_obs, np.s_[0:54])
-        obs = np.delete(main_obs, np.s_[0:18])
-        obs = np.delete(obs, np.s_[9:18])
-
-        obs = np.delete(obs, np.s_[27:29])
-        obs = np.delete(obs, np.s_[28])
-
-        obs = np.delete(obs, np.s_[36:42])
-        obs = np.delete(obs, np.s_[39:42])
-        obs = np.delete(obs, np.s_[42:45])
-
-        #obs = np.delete(obs, np.s_[63:72])
-        #obs = np.delete(obs, np.s_[36:54])
-
-    return obs
-
 # Get dictionary from baselines/ppo2/defaults
 defaults = get_learn_function_defaults('ppo2', 'phantomx_mlp')
 
@@ -187,7 +92,7 @@ def runner(leg, env):
                     nbatch_train=nbatch_train,
                     nsteps=defaults['nsteps'], ent_coef=defaults['ent_coef'], vf_coef=defaults['vf_coef'],
                     max_grad_norm=defaults['max_grad_norm'])
-    model.load('/tmp/training_data/dockerv1.3/PhantomX-v0/dppo2_mlp/2019-12-03_17h05min/' + leg + '/checkpoints/07030')
+    model.load('/tmp/training_data/dockerv1.3/PhantomX-v0/dppo2_mlp/2019-12-03_17h05min/' + leg + '/checkpoints/07000')
     obs = leg_env.reset()
     while True:
         action, value_estimate, next_state, neglogp = model.step(obs)
